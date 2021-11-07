28014288a72fde3f899b2986898dc4b17d664367
Derek Brottlund <derekbrottlund@gmail.com>
2021-11-07 16:12:41 -0500

    fix title

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home(props) {
 
   return (
     <main>
-      <Metatags title="Home Page" description="Get the latest posts on our site" />
+      <Metatags title="💡 Next.js + Firebase - Example App" description="Get the latest posts on our site" />
 
       <div className="card card-info">
         <h2>💡 Next.js + Firebase - Example App</h2>
